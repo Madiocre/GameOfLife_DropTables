8c26284ea964d738094a0cb1a276f48a71c57063
Justxd22 <xdjust18@gmail.com>
2024-10-15 01:52:56 +0300

    Add 4 patterns

--- gol.py
+++ gol.py
@@ -4,6 +4,7 @@ from tkinter import Label, Button, Canvas, Frame, Scale, filedialog
 import pygame
 from pygame import mixer
 from utils import resource_path
+import random
 
 class GameOfLife:
     """
@@ -135,6 +136,10 @@ class GameOfLife:
 
     def initialize_grid(self):
         self.on_resize(None)
+        self.clear_grid()
+        patterns = ["Gosper Glider Gun", "Pulse", "Cooper Head", "Infinite"]
+        chosen_pattern = random.choice(patterns)
+        self.create_pattern(chosen_pattern)
         self.draw_grid()
 
     def on_resize(self, event):
@@ -247,6 +252,8 @@ class GameOfLife:
 
         self.mute_button = Button(self.control_panel, text="Mute", command=self.mute, **button_style)
         self.mute_button.pack(side='left', padx=5, pady=5)
+        self.pattern_label = Label(self.control_panel, text="Pattern: None", bg=self.color_palette["primary"], fg=self.color_palette["accent"])
+        self.pattern_label.pack(side='left', padx=5, pady=5)
 
         self.speed_scale = Scale(self.control_panel, from_=1, to=10, orient='horizontal', label='Speed',
                                  command=self.update_speed, **scale_style)
@@ -272,21 +279,32 @@ class GameOfLife:
             with open(file_path, "w") as f:
                 json.dump(state, f)
 
-    def load_state(self):
-        file_path = filedialog.askopenfilename(filetypes=[("JSON files", "*.json"), ("All files", "*.*")])
-        if file_path:
-            with open(file_path, "r") as f:
-                state = json.load(f)
-            
-            self.cell_size = state["cell_size"]
-            self.grid = {tuple(cell): 1 for cell in state["grid"]}
-            self.width = state["width"]
-            self.height = state["height"]
-            # Update the grid size scale
-            self.grid_size_scale.set(self.cell_size)
-            # Redraw the grid
-            self.draw_grid()
+    def load_state(self, state=None):
+        if not state:
+            file_path = filedialog.askopenfilename(filetypes=[("JSON files", "*.json"), ("All files", "*.*")])
+            if file_path:
+                with open(file_path, "r") as f:
+                    state = json.load(f)
 
+        self.cell_size = state["cell_size"]
+        self.grid = {tuple(cell): 1 for cell in state["grid"]}
+        self.width = state["width"]
+        self.height = state["height"]
+        # Update the grid size scale
+        self.grid_size_scale.set(self.cell_size)
+        # Redraw the grid
+        self.draw_grid()
+
+    def create_pattern(self, pattern_name):
+        self.pattern_label.config(text=f"Pattern: {pattern_name}")
+        if pattern_name == "Gosper Glider Gun":
+            self.create_gosper_glider_gun()
+        elif pattern_name == "Pulse":
+            self.create_pluse()
+        elif pattern_name == "Cooper Head":
+            self.create_copperhead()
+        elif pattern_name == "Infinite":
+            self.create_Infinite()
 
     def update_grid_size(self, val):
         new_cell_size = int(val)
@@ -326,9 +344,10 @@ class GameOfLife:
             for j in range(-1, 2):
                 if i == 0 and j == 0:
                     continue
-                r = (row + i) % self.height
-                c = (col + j) % self.width
-                count += (r, c) in self.grid
+                r = row + i
+                c = col + j
+                if 0 <= r < self.height and 0 <= c < self.width:
+                    count += (r, c) in self.grid
         return count
 
     def clear_grid(self):
@@ -337,3 +356,19 @@ class GameOfLife:
 
     def update_speed(self, val):
         self.speed = float(val)
+
+    def create_gosper_glider_gun(self):
+        data = {'cell_size': 16, 'grid': [[11, 3], [11, 4], [12, 4], [12, 3], [11, 13], [12, 13], [13, 13], [10, 14], [9, 15], [9, 16], [14, 14], [15, 15], [15, 16], [12, 17], [10, 18], [11, 19], [12, 19], [13, 19], [14, 18], [12, 20], [11, 23], [10, 23], [9, 23], [9, 24], [10, 24], [11, 24], [12, 25], [8, 25], [8, 27], [7, 27], [12, 27], [13, 27], [9, 37], [10, 37], [10, 38], [9, 38]], 'width': 43, 'height': 26}
+        self.load_state(data)
+
+    def create_pluse(self):
+        state = {'cell_size': 16, 'grid': [[7, 12], [7, 13], [8, 12], [9, 12], [8, 11], [9, 11], [10, 11], [7, 15], [7, 16], [8, 16], [9, 16], [9, 17], [8, 17], [10, 17], [12, 13], [11, 13], [13, 13], [11, 8], [12, 8], [13, 8], [11, 15], [12, 15], [13, 15], [14, 16], [14, 17], [14, 18], [11, 20], [12, 20], [13, 20], [14, 12], [14, 11], [14, 10], [16, 10], [16, 11], [16, 12], [16, 16], [16, 17], [16, 18], [17, 20], [18, 20], [19, 20], [17, 8], [18, 8], [19, 8], [21, 10], [21, 11], [21, 12], [21, 16], [21, 17], [21, 18], [19, 13], [18, 13], [17, 13], [17, 15], [18, 15], [19, 15], [7, 30], [7, 31], [7, 32], [7, 36], [7, 37], [7, 38], [9, 35], [10, 35], [11, 35], [9, 33], [10, 33], [11, 33], [9, 40], [10, 40], [11, 40], [9, 28], [10, 28], [11, 28], [12, 30], [12, 31], [12, 32], [12, 36], [12, 37], [12, 38], [14, 36], [14, 37], [14, 38], [14, 32], [14, 31], [14, 30], [15, 33], [16, 33], [17, 33], [15, 35], [16, 35], [17, 35], [15, 40], [16, 40], [17, 40], [15, 28], [16, 28], [17, 28], [19, 30], [19, 31], [19, 32], [19, 36], [19, 37], [19, 38]], 'width': 44, 'height': 32}
+        self.load_state(state)
+
+    def create_copperhead(self):
+        state = {'cell_size': 20, 'grid': [[9, 6], [10, 6], [9, 7], [10, 7], [8, 9], [9, 9], [10, 9], [11, 9], [8, 10], [7, 10], [12, 10], [11, 10], [13, 11], [6, 11], [6, 13], [6, 14], [8, 14], [11, 14], [9, 15], [9, 16], [10, 16], [10, 15], [13, 14], [13, 13], [11, 17], [12, 17], [8, 17], [7, 17]], 'width': 35, 'height': 21}
+        self.load_state(state)
+
+    def create_Infinite(self):
+        state = {'cell_size': 5, 'grid': [[56, 82], [56, 84], [55, 84], [54, 86], [53, 86], [52, 86], [53, 88], [52, 88], [51, 88], [52, 89]], 'width': 320, 'height': 153}
+        self.load_state(state)
