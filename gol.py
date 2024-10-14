4028726925b262e2ca271b84dab0ea89c543ec10
Abdelrahman Mohamed <3bdelrahmanm0o@gmail.com>
2024-10-14 17:09:58 +0300

    Add documentation and refactor gol.py for improved readability and functionality
    - Added class-level and method-level docstrings to the GameOfLife class for better code documentation.
    - Documented the purpose, parameters, and functionality of each method.
    - Refactored the initialize_audio method to improve readability and initialization of audio components.
    - Refactored the setup_ui method to improve readability and setup of UI components.
    - Refactored the bind_events method to bind necessary events for the application.
    - Improved the draw_grid method to use more descriptive variable names and consistent formatting.
    - Enhanced the fill_cells_between method to handle cell selection more efficiently.
    - Updated the toggle_cell method to play appropriate sounds when toggling cell states.
    - Ensured consistent formatting and adherence to PEP 8 guidelines.

--- gol.py
+++ gol.py
@@ -1,5 +1,4 @@
 import tkinter as tk
-# from tkinter import Label, Button, Canvas, Frame, Scale
 from tkinter import Button, Frame, Scale
 import pygame
 from pygame import mixer
@@ -7,15 +6,35 @@ from utils import resource_path
 
 
 class GameOfLife:
+    """
+    A class to represent the Game of Life application.
+    """
+
     def __init__(self, master):
+        """
+        Initialize the Game of Life application.
+
+        Args:
+            master (tk.Tk): The root window of the application.
+        """
         self.master = master
         self.cell_size = 20
         self.is_running = False
-        self.speed = 1
+        self.speed = 5
         self.is_selecting = False
         self.last_cell = None
+        self.active_cells = set()
 
-        # Initialize pygame mixer
+        self.initialize_audio()
+        self.setup_ui()
+        self.bind_events()
+
+        self.master.after(100, self.initialize_grid)
+
+    def initialize_audio(self):
+        """
+        Initialize the audio components for the application.
+        """
         pygame.mixer.init()
         self.background_music = mixer.Sound(
             resource_path("assets/music/s3.wav")
@@ -23,44 +42,54 @@ class GameOfLife:
         self.click_sound = mixer.Sound(resource_path("assets/music/s2.wav"))
         self.remove_sound = mixer.Sound(resource_path("assets/music/s1.wav"))
 
-        # self.background_music.play(-1)  # loop
-
-        # frame on top of grid Canvas to contain the controls
+    def setup_ui(self):
+        """
+        Set up the user interface components for the application.
+        """
         self.frame = Frame(self.master)
         self.frame.pack(fill="both", expand=True)
 
         self.canvas = tk.Canvas(self.frame)
         self.canvas.pack(fill="both", expand=True)
 
-        # Store active cells in a set
-        self.active_cells = set()
-
         self.create_control_panel()
 
+    def bind_events(self):
+        """
+        Bind the necessary events for the application.
+        """
         self.master.bind("<Configure>", self.on_resize)
         self.canvas.bind("<Button-1>", self.start_selection)
         self.canvas.bind("<B1-Motion>", self.update_selection)
         self.canvas.bind("<ButtonRelease-1>", self.end_selection)
 
-        self.master.after(100, self.initialize_grid)
-
     def initialize_grid(self):
+        """
+        Initialize the grid for the Game of Life.
+        """
         self.on_resize(None)
         self.draw_grid()
 
     def on_resize(self, event):
+        """
+        Handle the window resize event.
+
+        Args:
+            event (tk.Event): The resize event.
+        """
         self.width = self.canvas.winfo_width() // self.cell_size
         self.height = self.canvas.winfo_height() // self.cell_size
         self.draw_grid()
 
     def draw_grid(self):
+        """
+        Draw the grid for the Game of Life.
+        """
         self.canvas.delete("all")
         for i in range(self.height):
             for j in range(self.width):
-                x1 = j * self.cell_size
-                y1 = i * self.cell_size
-                x2 = x1 + self.cell_size
-                y2 = y1 + self.cell_size
+                x1, y1 = j * self.cell_size, i * self.cell_size
+                x2, y2 = x1 + self.cell_size, y1 + self.cell_size
                 fill = "black" if (i, j) in self.active_cells else "white"
                 self.canvas.create_rectangle(
                     x1,
@@ -72,18 +101,26 @@ class GameOfLife:
                     )
 
     def start_selection(self, event):
-        # on box drag + hover
+        """
+        Start the cell selection process.
+
+        Args:
+            event (tk.Event): The mouse click event.
+        """
         self.is_selecting = True
         self.last_cell = (event.y // self.cell_size, event.x // self.cell_size)
         self.toggle_cell(event)
 
     def update_selection(self, event):
-        # while hovering
+        """
+        Update the cell selection process.
+
+        Args:
+            event (tk.Event): The mouse drag event.
+        """
         if self.is_selecting:
             current_cell = (
-                event.y //
-                self.cell_size, event.x //
-                self.cell_size
+                event.y // self.cell_size, event.x // self.cell_size
                 )
             self.fill_cells_between(self.last_cell, current_cell)
             self.last_cell = current_cell
@@ -91,21 +128,22 @@ class GameOfLife:
             self.click_sound.play()
 
     def fill_cells_between(self, start, end):
-        # calc new boxes enabled
-        # use parent box as start point
-        # calc coords distance
-        # highlight new selected if it's at that location
+        """
+        Fill the cells between the start and end points.
+
+        Args:
+            start (tuple): The starting cell coordinates.
+            end (tuple): The ending cell coordinates.
+        """
         x0, y0 = start
         x1, y1 = end
-        dx = abs(x1 - x0)
-        dy = abs(y1 - y0)
-        sx = 1 if x0 < x1 else -1
-        sy = 1 if y0 < y1 else -1
+        dx, dy = abs(x1 - x0), abs(y1 - y0)
+        sx, sy = 1 if x0 < x1 else -1, 1 if y0 < y1 else -1
         err = dx - dy
 
         while True:
             if 0 <= x0 < self.height and 0 <= y0 < self.width:
-                self.grid[x0][y0] = 1
+                self.active_cells.add((x0, y0))
             if x0 == x1 and y0 == y1:
                 break
             e2 = 2 * err
@@ -117,110 +155,128 @@ class GameOfLife:
                 y0 += sy
 
     def end_selection(self, event):
-        # click relase
+        """
+        End the cell selection process.
+
+        Args:
+            event (tk.Event): The mouse release event.
+        """
         self.is_selecting = False
-        # self.click_sound.play()
 
     def toggle_cell(self, event):
-        col = event.x // self.cell_size
-        row = event.y // self.cell_size
+        """
+        Toggle the state of a cell.
+
+        Args:
+            event (tk.Event): The mouse click event.
+        """
+        col, row = event.x // self.cell_size, event.y // self.cell_size
         if 0 <= row < self.height and 0 <= col < self.width:
-            if (row, col) in self.active_cells:
-                self.active_cells.remove((row, col))
+            cell = (row, col)
+            if cell in self.active_cells:
+                self.active_cells.remove(cell)
                 self.remove_sound.play()
             else:
-                self.active_cells.add((row, col))
+                self.active_cells.add(cell)
                 self.click_sound.play()
             self.draw_grid()
 
     def create_control_panel(self):
-        self.control_panel = Frame(self.frame, bg=None)
+        """
+        Create the control panel for the application.
+        """
+        self.control_panel = Frame(self.frame)
         self.control_panel.pack(side='bottom', fill='x')
 
-        self.play_pause_button = Button(
-            self.control_panel, text="Play",
-            command=self.toggle_play_pause
-            )
-        self.play_pause_button.pack(side='left', padx=5, pady=5)
+        buttons = [
+            ("Play", self.toggle_play_pause),
+            ("Next Frame", self.next_frame),
+            ("Clear", self.clear_grid)
+        ]
 
-        self.next_frame_button = Button(
-            self.control_panel,
-            text="Next Frame",
-            command=self.next_frame
-            )
-        self.next_frame_button.pack(side='left', padx=5, pady=5)
-
-        self.clear_button = Button(
-            self.control_panel,
-            text="Clear",
-            command=self.clear_grid
-            )
-        self.clear_button.pack(side='left', padx=5, pady=5)
+        for text, command in buttons:
+            Button(self.control_panel, text=text, command=command).pack(
+                side='left',
+                padx=5,
+                pady=5
+                )
 
         self.speed_scale = Scale(
             self.control_panel,
-            from_=1,
-            to=10,
-            orient='horizontal',
-            label='Speed',
+            from_=1, to=10, orient='horizontal', label='Speed',
             command=self.update_speed
-            )
-        self.speed_scale.set(5)  # Set default speed to middle value
+        )
+        self.speed_scale.set(self.speed)
         self.speed_scale.pack(side='right', padx=5, pady=5)
 
     def toggle_play_pause(self):
+        """
+        Toggle the play/pause state of the game.
+        """
         self.is_running = not self.is_running
-        self.play_pause_button.config(
+        self.control_panel.children['!button'].config(
             text="Pause" if self.is_running else "Play"
             )
-        self.master.after(10, self.draw_grid)
         if self.is_running:
             self.run_game()
 
     def run_game(self):
+        """
+        Run the game loop.
+        """
         if self.is_running:
             self.next_frame()
             self.master.after(int(1000 / self.speed), self.run_game)
 
     def next_frame(self):
+        """
+        Calculate the next frame of the game.
+        """
         new_active_cells = set()
-        cells_to_check = set(self.active_cells)
-
-        # Add all neighbors of active cells to the cells to check
-        for cell in self.active_cells:
-            cells_to_check.update(self.get_neighbors(*cell))
+        cells_to_check = self.active_cells.union(
+            *map(self.get_neighbors, self.active_cells)
+            )
 
         for cell in cells_to_check:
-            neighbors = self.count_neighbors(*cell)
-            if cell in self.active_cells:
-                if neighbors in [2, 3]:
-                    new_active_cells.add(cell)
-            else:
-                if neighbors == 3:
-                    new_active_cells.add(cell)
+            neighbors = sum(
+                neighbor in self.active_cells
+                for neighbor in self.get_neighbors(cell)
+                )
+            if neighbors == 3 or (
+                cell in self.active_cells and neighbors == 2
+                    ):
+                new_active_cells.add(cell)
 
         self.active_cells = new_active_cells
         self.draw_grid()
 
-    def count_neighbors(self, row, col):
-        count = 0
-        for neighbor in self.get_neighbors(row, col):
-            if neighbor in self.active_cells:
-                count += 1
-        return count
+    def get_neighbors(self, cell):
+        """
+        Get the neighbors of a cell.
 
-    def get_neighbors(self, row, col):
-        neighbors = []
-        for i in range(-1, 2):
-            for j in range(-1, 2):
-                if i == 0 and j == 0:
-                    continue
-                neighbors.append(((row + i), (col + j)))
-        return neighbors
+        Args:
+            cell (tuple): The cell coordinates.
+
+        Returns:
+            set: A set of neighboring cell coordinates.
+        """
+        row, col = cell
+        return {((row + i) % self.height, (col + j) % self.width)
+                for i in (-1, 0, 1) for j in (-1, 0, 1)
+                if (i, j) != (0, 0)}
 
     def clear_grid(self):
+        """
+        Clear the grid.
+        """
         self.active_cells.clear()
         self.draw_grid()
 
     def update_speed(self, val):
+        """
+        Update the speed of the game.
+
+        Args:
+            val (str): The new speed value.
+        """
         self.speed = float(val)
